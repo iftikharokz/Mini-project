112fcb7f28b6d7dd318c661f4d94f35748dc774e
iftikharokz <73760345+iftikharokz@users.noreply.github.com>
2020-12-08 16:04:05 +0500

    Update spell_checker.py

--- spell_checker.py
+++ spell_checker.py
@@ -4,7 +4,7 @@ spell=SpellChecker() # create the spell object to call SpellChecker class
 
 misspelled_word=spell.unknown(["amerca","fridy","universty","schol","student","houe"]) # "unknown" is the method by which we will get the word not found in dictionary
 
-print(misspelled_word) # printing the missspelled word
+print(misspelled_word) # printing the missspelled word => the word "student" will not be print out as it already correct
 
 for word in misspelled_word: # "for" loop for the iteration of list
 
